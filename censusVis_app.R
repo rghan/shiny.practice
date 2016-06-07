99c4a1ee657d19bbe36170244eb7b88fdf76d8cf
rghan <rghan@nevada.unr.edu>
2016-06-07 08:41:23 -0700

    fixed column width, added tutorial url

--- censusVis_app.R
+++ censusVis_app.R
@@ -3,25 +3,22 @@ ui <- fluidPage(
 
   titlePanel("CensusVis"),
 
-  br(),
+  HTML("<p>App interface based on the <a href='http://shiny.rstudio.com/tutorial/lesson3/', target='blank'>Rstudio tutorial-3</a>.</p>"),
+
   br(),
 
   fluidRow(
-    column(
+    column(4,
       h3("Help text"),
       helpText("Create demographic maps with information from the,
-        2010 US Census.")),
-    
-    column(
+        2010 US Census."),
       selectInput("select", label = h3("Choose a variable to display"), 
         choices = list("Percent White" = 1, "Percent Black" = 2,
-                       "Percent Hispanic" = 3, "Percent Asian" = 4), selected = 1)),
-    
-    column(
+                       "Percent Hispanic" = 3, "Percent Asian" = 4), selected = 1),
       sliderInput("slider1", label = h3("Range of Interest:"),
         min = 0, max = 100, value = c(0, 100))
       )
-    )
+  )
 )
 
 server <- function(input, output) {
